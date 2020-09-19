e7c3080b9bc3299ccca8466825a4190054e39f37
Clint Lawson <clint_lwsn@hotmail.com>
2020-09-19 11:31:14 -0600

    second commit

---
+++ client/build/precache-manifest.3816e5d4e1bef4f87644f82822fc7678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2447e907ef5785f80f1ce382237b9e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1673a56dde067e0d405",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4462564a2b35bdfd2e48",
+    "url": "/static/js/2.003f955e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.003f955e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1673a56dde067e0d405",
+    "url": "/static/js/main.cfc9c1c2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
